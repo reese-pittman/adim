f5b791a3b051942f5b6fd04f01f201114292e6ce
reese-pittman <reeseaverypittman@gmail.com>
2025-04-11 00:46:51 -0500

    Nav&SideNav are working. No css

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const SideNav = (isActive = false, changeActive) => {
+
+    const testingClick = () => alert();
+
+
+
+    return(
+        <nav className={`sideNav ${isActive ? "open" : ""}`}>
+            <h2>Pages</h2>
+            <ul>
+                <li><Link to="/" className="logo"> Home </Link></li>
+                <li><Link to="/search" className="Search"> Search </Link></li>
+                <li><Link to="/map" className="Map"> Map </Link></li>
+            </ul>
+            <button className="sideNav_close_btn" onClick={() =>testingClick} >Close</button>
+        </nav>
+    );
+};
+
+export default SideNav
