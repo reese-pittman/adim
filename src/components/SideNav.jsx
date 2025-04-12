a0d4702047a473d8ee93839bfefc44afd3b8f617
reese-pittman <reeseaverypittman@gmail.com>
2025-04-11 23:43:35 -0500

    Little fix to SideNav.

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const SideNav = (isActive = false, changeActive) => {
 
-    const testingClick = () => alert();
 
 
 
+    
     return(
         <nav className={`sideNav ${isActive ? "open" : ""}`}>
             <h2>Pages</h2>
@@ -14,7 +14,7 @@ const SideNav = (isActive = false, changeActive) => {
                 <li><Link to="/search" className="Search"> Search </Link></li>
                 <li><Link to="/map" className="Map"> Map </Link></li>
             </ul>
-            <button className="sideNav_close_btn" onClick={() =>testingClick} >Close</button>
+            <button className="sideNav_close_btn" onClick={() => changeActive} >Close</button>
         </nav>
     );
 };
